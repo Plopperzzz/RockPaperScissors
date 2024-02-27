354377e602daa47257810ac4fbd9db32aaaaa233
Christian Anderson <christian.anderson982@gmail.com>
2024-02-26 19:00:54 -0500

    not sure what I changed ...

--- Scripts/RockPaperScissors.js
+++ Scripts/RockPaperScissors.js
@@ -2,11 +2,10 @@ function getComputerChoice(choices=['Rock', 'Paper', 'Scissors'],weights=[1/3, 1
     let epsilon = 10e-5;
     let num, cutoffValue = 0;
 
-    console.log(Math.abs(weights.reduce((a,b)=>a+b,0) - 1));
     if(Math.abs(weights.reduce((a,b)=>a+b,0) - 1) > epsilon )
     {
         console.error("Weights do not sum to unity");
-        return;
+        return 1;
     }
     if(weights.length != choices.length)
     {
@@ -16,10 +15,7 @@ function getComputerChoice(choices=['Rock', 'Paper', 'Scissors'],weights=[1/3, 1
     num = Math.random();
     for(let i = 0; i < choices.length; ++i)
     {
-        console.log('i: '+i);
-        console.log('num: '+num);
         cutoffValue += weights[i];
-        console.log('cutoff: '+cutoffValue);
 
         if(num - weights[i] < epsilon)
         {
